6027383de68eca7058b94f23157ae5cfd083ec5e
nillu8147466089 <nileshkumar.singh2@in.bosch.com>
2024-04-29 11:18:26 +0530

    test

---
+++ src/main/java/com/bosch/spring/microservices/springcloudcofigserver/SpringCloudCofigServerApplication.java
@@ -0,0 +1,13 @@
+package com.bosch.spring.microservices.springcloudcofigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCofigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCofigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bosch/spring/microservices/springcloudcofigserver/SpringCloudCofigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosch.spring.microservices.springcloudcofigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCofigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
